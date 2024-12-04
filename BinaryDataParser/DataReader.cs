2304a5e381e908b6a94d22bf83fe97dbe13ed6f1
SbWereWolf <ulfnew@gmail.com>
2024-12-04 11:31:05 +0500

    Add binary file reader

---
+++ BinaryDataParser/DataReader.cs
@@ -0,0 +1,66 @@
+
+using System.IO;
+
+namespace BinaryDataParser
+{
+    public class DataReader
+    {
+        private readonly FileStream? Source;
+        private readonly long FileLength = 0;
+
+        public DataReader(FileStream source, long length)
+        {
+            Source = source;
+            FileLength = length;
+        }
+
+        public bool MayBackward(long dataFrameSize)
+        {
+            var allow = !(Source == null && Source?.Position == dataFrameSize);
+
+            return allow;
+        }
+
+        public bool MayForward(long dataFrameSize)
+        {
+            var allow = !(Source == null && Source?.Position == FileLength);
+
+            return allow;
+        }
+
+        public int Backward(byte[] dataFrame)
+        {
+            int bytesRead = 0;
+            if (Source != null)
+            {
+                var mayBackward = Source.Position >= dataFrame.Length * 2;
+                long position = 0;
+                if (mayBackward)
+                {
+                    position = -2 * dataFrame.LongLength;
+                }
+                if (!mayBackward)
+                {
+                    position = -1 * Source.Position;
+                }
+
+                Source.Seek(position, SeekOrigin.Current);
+                bytesRead = Source.Read(dataFrame);
+            }
+
+            return bytesRead;
+        }
+
+        public int Forward(byte[] dataFrame)
+        {
+            int bytesRead = 0;
+            if (Source != null)
+            {
+                bytesRead = Source.Read(dataFrame);
+            }
+
+            return bytesRead;
+        }
+
+    }
+}
